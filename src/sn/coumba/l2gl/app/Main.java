c6c443915fd843a3f12dce8b6dad8a4201a8a5f9
Coumbaa <coumbandiaye2605@gmail.com>
2026-02-21 00:48:27 +0000

    Update Main.java

--- src/sn/coumba/l2gl/app/Main.java
+++ src/sn/coumba/l2gl/app/Main.java
@@ -49,6 +49,23 @@ public class Main {
             // Tentons de créer une note invalide pour tester notre protection
             System.out.println("\n--- Test de sécurité (Note > 20) ---");
             Note nErreur = new Note(e1, m1, 25.0); // Ceci va échouer et aller au catch
+             // Création de deux étudiants pour tester le compteur
+            Etudiant e1 = new Etudiant("2026-001", "Awa");
+            Etudiant e2 = new Etudiant("2026-002", "Moussa");
+            
+            Module m1 = new Module("JAVA", "Programmation Java", 2.0);
+            
+            // Création de la dernière note
+            Note n1 = new Note(e1, m1, 17.5);
+
+            // ÉTAPE 4 : Affichage du Bilan Final
+            // Notez l'appel Etudiant.getCompteur() (avec une Majuscule)
+            System.out.println("\n========================================");
+            System.out.println("BILAN TD2:");
+            System.out.print("Etudiants créés: " + Etudiant.getCompteur());
+            System.out.print(" | Dernière note: " + n1.getValeur() + "/20");
+            System.out.println(" | Points (coeff): " + n1.getPoints());
+            System.out.println("========================================");
 
         } catch (IllegalArgumentException e) {
             // ÉTAPE 9 : Capture et affichage du message d'erreur si une règle est violée
